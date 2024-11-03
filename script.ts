14b32021ccde252fd2260eef1f2fe5f4272ad873
Huzaifa Aziz <azizh4495@gmail.com>
2024-11-03 23:06:04 +0500

    poerfolio

--- script.ts
+++ script.ts
@@ -3,18 +3,15 @@ console.log("ts running..");
 
 
 
-// Select elements by their IDs and class names
 const sidebarToggle = document.getElementById("sidebarToggle") as HTMLElement;
 const navbar = document.querySelector(".navbar") as HTMLElement;
 
-// Function to handle the sidebar toggle
 function toggleSidebar(): void {
     if (navbar) {
-        navbar.classList.toggle("active"); // Toggle the 'active' class to show/hide sidebar
+        navbar.classList.toggle("active"); 
     }
 }
 
-// Check if the sidebarToggle element exists to avoid any TypeScript null errors
 if (sidebarToggle) {
     sidebarToggle.addEventListener("click", toggleSidebar);
 }
